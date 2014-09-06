d4079e257cc050921b988a9cd1c36d232b2d73e6
princessning <yw5cm@virginia.edu>
2014-09-06 13:35:04 -0400

    Delete plot3.R

--- plot3.R
+++
@@ -1,7 +0,0 @@
-png(filename = "plot3.png",
-    width = 480, height = 480)
-plot(time1,usedata$Sub_metering_1,type='l',ylab='Energy sub metering',xlab='')
-lines(time1,usedata$Sub_metering_2,col='red')
-lines(time1,usedata$Sub_metering_3,col='blue')
-legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1,col=c('black','red','blue'))
-dev.off()
